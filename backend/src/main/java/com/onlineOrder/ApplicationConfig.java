c859bc4d2609466e0a1f8189b5a3c6a5c7d11f1e
lnsidiou5 <21jason.li@gmail.com>
2023-06-29 22:44:56 -0400

    Last update
    Verified that everything was functioning properly

--- backend/src/main/java/com/onlineOrder/ApplicationConfig.java
+++ backend/src/main/java/com/onlineOrder/ApplicationConfig.java
@@ -34,9 +34,9 @@ public class ApplicationConfig {
 
     @Bean(name = "dataSource")
     public DataSource dataSource() {
-        String RDS_ENDPOINT = "onlineordering-instance.cua0e1jdzudw.us-east-1.rds.amazonaws.com";
-        String USERNAME = "";
-        String PASSWORD = "";
+        String RDS_ENDPOINT = "onlineorder-instance.cnce3pbxkhna.us-west-1.rds.amazonaws.com";
+        String USERNAME = "admin";
+        String PASSWORD = "enterpassword";
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://" + RDS_ENDPOINT + ":3306/onlineOrder?createDatabaseIfNotExist=true&serverTimezone=UTC");
